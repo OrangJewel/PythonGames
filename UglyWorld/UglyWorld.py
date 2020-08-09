1553abe8c198181b52fb5428542e2f42edb48790
OrangJewel <ewan@danandlaurajuliano.com>
2020-08-09 16:27:53 -0500

    added extra toggleable text to 'Ugly World'

--- UglyWorld/UglyWorld.py
+++ UglyWorld/UglyWorld.py
@@ -14,11 +14,33 @@ textSurfaceObj = fontObj.render('Hello world', True, GREEN, BLUE)
 textRectObj = textSurfaceObj.get_rect()
 textRectObj.center = (200, 150)
 
+keySurfaceObj = fontObj.render('I hope to god this works', True, BLUE, GREEN)
+keyRectObj = keySurfaceObj.get_rect()
+keyRectObj.center = (200, 250)
+
+displayText = False
+
 while True:
     DISPLAYSURF.fill(WHITE)
     DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+    
+    if displayText == True:
+        DISPLAYSURF.blit(keySurfaceObj, keyRectObj)
+    
     for event in pygame.event.get():
-        if event.type ==QUIT:
+        if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == KEYUP:
+            if event.key == K_a:
+                print ('The a key has been pressed')
+                displayText = True
+            if event.key == K_d:
+                print ('The d key has been pressed')
+                displayText = False
+            if event.key == K_ESCAPE:
+                print ('The Esc key has been pressed')
+                pygame.quit()
+                sys.exit()
+                
     pygame.display.update()
