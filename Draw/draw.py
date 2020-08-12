52e83a6f24b46b34da088581a4532a841e905a68
OrangJewel <ewan@danandlaurajuliano.com>
2020-08-11 19:03:03 -0500

    Learned how to make things transparent.

--- Draw/draw.py
+++ Draw/draw.py
@@ -13,16 +13,26 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = ( 0, 255, 0)
 BLUE = ( 0, 0, 255)
+BEIGE = (245, 245, 220)
+SBLUE = ( 0, 0, 255, 50)
+SBEIGE = (245, 245, 220, 0)
 
 #draw on the surface object
-DISPLAYSURF.fill(WHITE)
-pygame.draw.polygon(DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+DISPLAYSURF.fill(BEIGE)
+pygame.draw.polygon(DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106), (90, 90)))
 pygame.draw.line(DISPLAYSURF, BLUE, (60, 60), (120, 60), 4)
 pygame.draw.line(DISPLAYSURF, BLUE, (120, 60), (60, 120))
 pygame.draw.line(DISPLAYSURF, BLUE, (60, 120), (120, 120), 4)
-pygame.draw.circle(DISPLAYSURF, BLUE, (300, 50), 20, 0)
+pygame.draw.circle(DISPLAYSURF, SBLUE, (146, 149), 20, 0)
+pygame.draw.circle(DISPLAYSURF, SBEIGE, (146, 149), 8, 0)
 pygame.draw.ellipse(DISPLAYSURF, RED, (300, 250, 40, 80), 1)
 pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
+
+# draw a semi-transparent shape
+blue_image = pygame.Surface((50, 50), pygame.SRCALPHA)
+#pygame.draw.rect(blue_image, SBLUE, blue_image.get_rect(), 10)
+pygame.draw.circle(blue_image, SBLUE, (25, 25), 27, 0)
+DISPLAYSURF.blit(blue_image, (75, 175))
 
 pix0bj = pygame.PixelArray(DISPLAYSURF)
 pix0bj[480][380] = BLACK
