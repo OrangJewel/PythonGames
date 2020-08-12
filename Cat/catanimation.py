b6dbd5c2d127fada9e91036e6fb258659fa52c4d
OrangJewel <ewan@danandlaurajuliano.com>
2020-08-11 19:43:59 -0500

    A dog is now chasing the cat

--- Cat/catanimation.py
+++ Cat/catanimation.py
@@ -14,28 +14,52 @@ WHITE = (255, 255, 255)
 catImg = pygame.image.load('cat.png')
 catx = 10
 caty = 10
-direction = 'right'
+cdirection = 'right'
+
+dogpic = pygame.image.load('doggo.png')
+dogImg = pygame.transform.scale(dogpic, (70, 70))
+dogx = -100
+dogy = 0
+ddirection = 'dright'
 
 while True: # the main game loop
     DISPLAYSURF.fill(WHITE)
 
-    if direction == 'right':
+    if cdirection == 'right':
         catx += 5
         if catx == 280:
-            direction = 'down'
-    elif direction == 'down':
+            cdirection = 'down'
+    elif cdirection == 'down':
         caty += 5
         if caty == 220:
-            direction = 'left'
-    elif direction == 'left':
+            cdirection = 'left'
+    elif cdirection == 'left':
         catx -= 5
         if catx == 10:
-            direction = 'up'
-    elif direction == 'up':
+            cdirection = 'up'
+    elif cdirection == 'up':
         caty -= 5
         if caty == 10:
-            direction = 'right'
+            cdirection = 'right'
     DISPLAYSURF.blit(catImg, (catx, caty))
+
+    if ddirection == 'dright':
+        dogx += 5
+        if dogx == 280:
+            ddirection = 'ddown'
+    elif ddirection == 'ddown':
+        dogy += 5
+        if dogy == 220:
+            ddirection = 'dleft'
+    elif ddirection == 'dleft':
+        dogx -= 5
+        if dogx == 10:
+            ddirection = 'dup'
+    elif ddirection == 'dup':
+        dogy -= 5
+        if dogy == 10:
+            ddirection = 'dright'
+    DISPLAYSURF.blit(dogImg, (dogx, dogy))
 
     for event in pygame.event.get():
         if event.type == QUIT:
